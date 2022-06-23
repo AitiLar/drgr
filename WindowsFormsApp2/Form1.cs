e0ee1218d9524cd5b2790fe4aa936f4003edce0b
AitiLar <nparada@bk.ru>
2022-06-24 06:42:56 +0700

    commit one

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        PostirkaEntities db = new PostirkaEntities();
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.DataSource = db.Лист1_Staff.ToList();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+    }
+}
